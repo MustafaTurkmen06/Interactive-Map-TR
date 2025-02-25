f1df888de24ce0c3dea3d1447ab0cb8c6d6440c0
emxis <106862004+emxis@users.noreply.github.com>
2025-02-26 00:32:10 +0300

    Add files via upload
    cd frontend 
    npm i
    npm run dev

---
+++ turkey - Copy (12)/frontend/src/MainPage.jsx
@@ -0,0 +1,153 @@
+import React from 'react'
+import turkeyHigh from "./images/fly.png"
+// import rusevi from "./images/rusevi.png"
+import matrushka from "./images/matrushka.png"
+import decoration from "./images/homepage-offer-decoration.svg"
+import ornekDergi from "./images/ornekDergi.jpg"
+import ruseviBina from "./images/ruseviBina.jpg"
+const MainPage = () => {
+  return (
+    <div>
+     <div className='w-100 py-5 overflow-hidden mainPageFirst position-relative' style={{backgroundColor:"#e2e9ff"}}>
+     <div className='container px-5 pt-5 pb-5 '>
+        <div className='row g-0'>
+            <div className='col-lg'>
+            <h1 className='mainBaslik'>Rusya’da yüksek öğrenim: giriş, eğitim, kariyer</h1>
+                <div className='gradient-text'>
+                  <div className='d-flex '>
+                <h3 className='baslikAlt'>Хочешь учиться в России?</h3>
+                <img src={decoration} className='mt-2' style={{width:"20%"}} alt="" />
+                <h6 className='baslikAlt'>Выбери университет</h6>
+                  </div>
+                </div>  
+                <div className='d-flex gap-2'>
+                <a href="" className='btn rusEviButton pe-5 tgButton' style={{paddingLeft:"2.2rem"}}>Rusevi</a>
+                <a href="" className='btn tgButton'>Telegram <i class="fa-brands fa-telegram"></i></a>
+                </div>
+            </div>
+            <div className='col-lg'>
+            <img src={turkeyHigh} className='turkeyMapMain' style={{width:"120%"}} alt="" />
+            </div>
+        </div>
+        
+      </div>
+      <div class="position-absolute bottom-0 start-25 ms-5">
+        <img src={matrushka} alt="" style={{width:"15%"}}/>
+        </div>
+        <div class="position-absolute top-0 end-0 me-5 rotateMat">
+        <img src={matrushka} alt="" style={{width:"15%"}}/>
+        </div>
+        {/* dergiler */}
+        <div className='container mt-5 px-5'>
+          <div className='d-flex justify-content-between w-100'>
+          <h2 className='normalBaslik'>Rus Evi Ankara Dergisi</h2>
+          <button className='btn dergiButton px-3 py-2'>Tüm Sayılar <i className="fa-solid fa-arrow-right"></i></button>
+          </div>
+          {/* dergi row and col */}
+          <div className='row mt-4 gap-5'>
+          <a href="" className="text-decoration-none w-auto">
+          <div className="col-lg">
+            <div className="card border-0 cardBoxDergi bg-transparent" style={{ width: "15rem" }}>
+              <div className="cardDergiImgWrapper">
+                <img src={ornekDergi} className="card-img-top cardDergiImg" alt="..." />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title text-center dergiCardText">Martin Eden Dergisi</h5>
+              </div>
+            </div>
+          </div>
+           </a>
+           <a href="" className="text-decoration-none w-auto">
+          <div className="col-lg">
+            <div className="card border-0 cardBoxDergi bg-transparent" style={{ width: "15rem" }}>
+              <div className="cardDergiImgWrapper">
+                <img src={ornekDergi} className="card-img-top cardDergiImg" alt="..." />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title text-center dergiCardText">Martin Eden Dergisi</h5>
+              </div>
+            </div>
+          </div>
+           </a>
+           <a href="" className="text-decoration-none w-auto">
+          <div className="col-lg">
+            <div className="card border-0 cardBoxDergi bg-transparent" style={{ width: "15rem" }}>
+              <div className="cardDergiImgWrapper">
+                <img src={ornekDergi} className="card-img-top cardDergiImg" alt="..." />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title text-center dergiCardText">Martin Eden Dergisi</h5>
+              </div>
+            </div>
+          </div>
+           </a>
+          </div>
+        </div>
+     </div>
+     <div className='container mt-5 px-5'>
+     <div className='d-flex justify-content-between w-100'>
+          <h2 className='normalBaslik'>Bazı Kurumları Tanıyın!</h2>
+          <button className='btn dergiButton px-3 py-2'>Tüm Kurumlar <i className="fa-solid fa-arrow-right"></i></button>
+          </div>
+          <div className='row pt-4 g-5'>
+        <div className='col-lg-4'> 
+          <div className='kurumCard'>
+            <a href="">
+              <img src={ruseviBina} className='rounded-4' alt="" />
+              <div className='photo-dark-overlay'></div>
+              <div className='kurumCard-content'>
+                <div className='custom-fit p-2 d-inline-block ms-2'>
+                  <p className='d-inline'>Konsolosluk</p>
+                </div>
+                <div className='kurumCardYazilar'> 
+                  <h5>İstanbul Başkonsolusluğu</h5>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis perferendis labore velit repellat sit harum nisi praesentium, aspernatur asperiores.</p>
+                  <a href="" className='btn kurumButton'>Kurumu Görün!</a>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className='col-lg-4'> 
+          <div className='kurumCard'>
+            <a href="">
+              <img src={ruseviBina} className='rounded-4' alt="" />
+              <div className='photo-dark-overlay'></div>
+              <div className='kurumCard-content'>
+                <div className='custom-fit p-2 d-inline-block ms-2'>
+                  <p className='d-inline'>Konsolosluk</p>
+                </div>
+                <div className='kurumCardYazilar'> 
+                  <h5>İstanbul Başkonsolusluğu</h5>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis perferendis labore velit repellat sit harum nisi praesentium, aspernatur asperiores.</p>
+                  <a href="" className='btn kurumButton'>Kurumu Görün!</a>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className='col-lg-4'> 
+          <div className='kurumCard'>
+            <a href="">
+              <img src={ornekDergi} className='rounded-4' alt="" />
+              <div className='photo-dark-overlay'></div>
+              <div className='kurumCard-content'>
+                <div className='custom-fit p-2 d-inline-block ms-2'>
+                  <p className='d-inline'>Konsolosluk</p>
+                </div>
+                <div className='kurumCardYazilar'> 
+                  <h5>İstanbul Başkonsolusluğu</h5>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis perferendis labore velit repellat sit harum nisi praesentium, aspernatur asperiores.</p>
+                  <a href="" className='btn kurumButton'>Kurumu Görün!</a>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+</div>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
